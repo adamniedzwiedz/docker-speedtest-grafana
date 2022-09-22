b888dc8313722171b3cab53608707461ea721e20
Adam Niedzwiedz <niedzwiedz.adam@gmail.com>
2022-09-23 00:10:18 +0200

    Fixed issues with influxdb backup

--- scripts/create_influx_backup.sh
+++ scripts/create_influx_backup.sh
@@ -5,11 +5,12 @@ if [ ! ${BASEDIR:0:1} == / ]; then
   BASEDIR="$(pwd)/$BASEDIR"
 fi
 
-INFLUX_BACKUP_FOLDER="$(BASEDIR)/../influx-backup"
-INFLUX_BACKUP_FILE="$(BASEDIR)/../influx-backup.tar.gz"
+INFLUX_BACKUP_FOLDER="$BASEDIR/../influx-backup"
+INFLUX_BACKUP_FILE="$BASEDIR/../influx-backup.tar.gz"
 
-if [ -d "$INFLUX_BACKUP_FOLDER" ] && rm -rf "$INFLUX_BACKUP_FOLDER/*" || mkdir -p "$INFLUX_BACKUP_FOLDER"
-if [ -f "$INFLUX_BACKUP_FILE" ] && rm -f $INFLUX_BACKUP_FILE
+rm -rf $INFLUX_BACKUP_FOLDER/*
+rm -f $INFLUX_BACKUP_FILE
+
 DOCKER_INFLUX=`docker ps | grep influxdb | cut -c 1-12`
 docker exec "$DOCKER_INFLUX" influxd backup -portable /data-backup
-tar -czvf "$INFLUX_BACKUP_FILE" "$INFLUX_BACKUP_FOLDER/*"
+tar -czvf $INFLUX_BACKUP_FILE $INFLUX_BACKUP_FOLDER/*

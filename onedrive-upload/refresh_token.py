4b2986dc944856968c7bb09c861b5a9f6c955db5
Adam Niedzwiedz <niedzwiedz.adam@gmail.com>
2022-09-24 21:02:44 +0200

    Fixed issues

--- onedrive-upload/refresh_token.py
+++ onedrive-upload/refresh_token.py
@@ -9,6 +9,6 @@ TOKEN_FILE = os.getenv('TOKEN_FILE')
 if __name__ == "__main__":
     token = MsToken(TOKEN_FILE)
     token_value = token.read()
-    if token_value is None:
+    if token_value is not None:
         new_token_value = token.refresh(token_value['refresh_token'])
         token.save(new_token_value)
--- scripts/add_cron_jobs.sh
+++ scripts/add_cron_jobs.sh
@@ -5,16 +5,11 @@ if [ ! ${BASEDIR:0:1} == / ]; then
   BASEDIR="$(pwd)/$BASEDIR"
 fi
 
-TMP_CRON_FILE="$(mktemp)"
-crontab -l > TMP_CRON_FILE
-
-# add morning jobs
-echo "50 7 * * * /usr/bin/python3 \"$BASEDIR/../onedrive-upload/refresh_token.py\"" >> TMP_CRON_FILE
-echo "0 8 * * * sh \"$BASEDIR/create_influx_backup.sh\"" >> TMP_CRON_FILE
-echo "10 8 * * * sh \"$BASEDIR/upload.sh\"" >> TMP_CRON_FILE
-# add evening jobs
-echo "50 19 * * * /usr/bin/python3 \"$BASEDIR/../onedrive-upload/refresh_token.py\"" >> TMP_CRON_FILE
-echo "0 20 * * * sh \"$BASEDIR/create_influx_backup.sh\"" >> TMP_CRON_FILE
-echo "10 20 * * * sh \"$BASEDIR/upload.sh\"" >> TMP_CRON_FILE
-
-rm $TMP_CRON_FILE
+crontab -l | { cat; echo "# add morning jobs"; } | crontab -
+crontab -l | { cat; echo "50 7 * * * /usr/bin/python3 \"$BASEDIR/../onedrive-upload/refresh_token.py\""; } | crontab -
+crontab -l | { cat; echo "0 8 * * * sh \"$BASEDIR/create_influx_backup.sh\""; } | crontab -
+crontab -l | { cat; echo "10 8 * * * sh \"$BASEDIR/upload.sh\""; } | crontab -
+crontab -l | { cat; echo "# add evening jobs"; } | crontab -
+crontab -l | { cat; echo "50 19 * * * /usr/bin/python3 \"$BASEDIR/../onedrive-upload/refresh_token.py\""; } | crontab -
+crontab -l | { cat; echo "0 20 * * * sh \"$BASEDIR/create_influx_backup.sh\""; } | crontab -
+crontab -l | { cat; echo "10 20 * * * sh \"$BASEDIR/upload.sh\""; } | crontab -
